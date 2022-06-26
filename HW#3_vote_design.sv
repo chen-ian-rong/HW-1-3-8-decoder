734b5a1c394071239dfa2f4e4918ee8367b601d3
chen-ian-rong <103410294+chen-ian-rong@users.noreply.github.com>
2022-06-27 02:20:34 +0800

    Update HW#3_vote_design.sv

--- HW#3_vote_design.sv
+++ HW#3_vote_design.sv
@@ -1,26 +1,35 @@
-module blocking (
-clk,
-rst_n,
-a_i,
-b_i,
-a_o,
-b_o
-);
-input clk;
-input rst_n;
-input a_i, b_i;
-output a_o, b_o;
-reg a, b;
-assign a_o = a;
-assign b_o = b;
-always @(posedge clk or negedge rst_n) begin
-if (~rst_n) begin
-a = a_i;
-b = b_i;
-end
-else begin
-a = b;
-b = a;
-end
-end
-endmodule
+`timescale 1ns/1ps 
+
+    module voter_if_tb();
+
+    reg [3:0] I_tb;
+    wire [3:1] O_tb;
+
+    voter_if voter4P(.I(I_tb),.O(O_tb)); 
+
+    initial begin
+        #0  I_tb = 4'b0000; 
+        #10 I_tb = 4'b0000; 
+        #10 I_tb = 4'b0001;
+        #10 I_tb = 4'b0010;
+        #10 I_tb = 4'b0011;
+        #10 I_tb = 4'b0100;
+        #10 I_tb = 4'b0101;
+        #10 I_tb = 4'b0110;
+        #10 I_tb = 4'b0111;
+        #10 I_tb = 4'b1000;
+        #10 I_tb = 4'b1001;
+        #10 I_tb = 4'b1010;
+        #10 I_tb = 4'b1011;
+        #10 I_tb = 4'b1100;
+        #10 I_tb = 4'b1101;
+        #10 I_tb = 4'b1110;
+        #10 I_tb = 4'b1111;
+        #10 $finish;
+        end
+    initial
+        begin
+            $dumpfile("voter_if.vcd");
+            $dumpvars(0,voter_if_tb); 
+        end
+    endmodule
