5cb3f88ce47f8267ac3bdfacf9c15e91c11bf6ad
chen-ian-rong <103410294+chen-ian-rong@users.noreply.github.com>
2022-06-27 02:27:10 +0800

    Update HW#4_testbench.sv

--- HW#4_testbench.sv
+++ HW#4_testbench.sv
@@ -1,37 +1,42 @@
-module decoder_3_8_tb;
-reg E_tb;
-reg clka, clkb;
-reg [2:0] In_tb;
-wire [7:0] Out_tb;
-wire clka_out, clkb_out;
-decoder_3_8 decoder_1(.E(E_tb), .In(In_tb), .Out(Out_tb));
-clkgen clkgen_1(.clka(clka), .clkb(clkb), .clka_out(clka_out), .clkb_out(clkb_out));
+`timescale 1ns/1ps
+module RTL_tb;
+    reg clk;
+    reg rst_asyn;
+	  wire  [3:0] Q_out;
+    parameter OFFSET = 0;
+
+// clock process
+initial
+begin
+    #OFFSET;
+    forever
+    begin
+        clk = 1'b0;
+        #2 clk = 1'b1;
+        #2;
+    end
+end
+
+// reset process
+initial
+begin
+    #OFFSET;
+    forever
+    begin
+        rst_asyn = 1'b1;
+        #5   rst_asyn = 1'b0;
+        #50;
+    end
+end
+
+// clock & reset finish
 initial begin
-clka = 1'b0; clkb = 1'b0;
-end
-always begin 
-#10 clka = ~clka; 
-end
-always begin
-#10 clkb = ~clkb;
+        #60 $finish;
 end
 
 initial begin
-#0 E_tb = 0; In_tb = 3'b000;
-#10 E_tb = 1; In_tb = 3'b000;
-#10 E_tb = 1; In_tb = 3'b001;
-#10 E_tb = 1; In_tb = 3'b010;
-#10 E_tb = 1; In_tb = 3'b011;
-#10 E_tb = 1; In_tb = 3'b100;
-#10 E_tb = 1; In_tb = 3'b101;
-#10 E_tb = 1; In_tb = 3'b110;
-#10 E_tb = 1; In_tb = 3'b111;
-#10 $finish;
+	$dumpfile("RTL.vcd");
+	$dumpvars(0, RTL_tb);
 end
-
-initial begin
-$dumpfile("decoder.vcd");
-$dumpvars(0, decoder_1);
-$dumpvars(0, clkgen_1);
-end
+BCD RTL_tb(.clk, .rst_asyn, .Q_out);
 endmodule
